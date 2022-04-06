a11b025c2e0c3d32b9184c219529337a5f60050f
bofei-jing <bja39@sfu.ca>
2022-04-06 14:26:34 -0700

    deleted reference section

--- search-frontend/src/result.js
+++ search-frontend/src/result.js
@@ -1,12 +1,7 @@
-import React, { useState } from 'react';
-
-import Button from "react-bootstrap/Button";
+import React from 'react';
 import Card from "react-bootstrap/Card";
-import Collapse from "react-bootstrap/Collapse";
-import Container from "react-bootstrap/Container";
 
 function Result(props) {
-    const [open, setOpen] = useState(false);
     return (
         <Card>
             <Card.Body>
@@ -18,22 +13,6 @@ function Result(props) {
                 <Card.Text>
                     {props.abstract}
                 </Card.Text>
-                <Button
-                    onClick={() => setOpen(!open)}
-                    aria-controls="example-collapse-text"
-                    aria-expanded={open}
-                >
-                    Referenced by
-                </Button>
-                <Collapse in={open}>
-                    <div id="example-collapse-text">
-                        <Container>
-                            <Card.Link href="#">Card Link</Card.Link>
-                            <Card.Link href="#">Card Link</Card.Link>
-                            <Card.Link href="#">Card Link</Card.Link>
-                        </Container>
-                    </div>
-                </Collapse>
             </Card.Body>
         </Card >
     )
